fbc96c612d0234c5bbeae7d2122b7c05a527c998
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2016-06-15 15:20:00 +0200

    API :id middelware

--- app/services/http/httpService.js
+++ app/services/http/httpService.js
@@ -15,6 +15,11 @@ module.exports = httpService
 
 function httpService() {
 
+    /**
+     * Middlewares
+     */
+
+    // Allow cross domain requests
     api.use(function(req, res, next)
     {
         res.header("Access-Control-Allow-Origin", "*")
@@ -22,26 +27,46 @@ function httpService() {
         next()
     })
 
+    // Access log
     api.use(function (req, res, next) {
       message_service.publish('log', `${req.method} ${req.url}`)
       next()
     });
 
+    // Get the thing
+    api.param('id', function(req, res, next, id) {
+        try {
+            req.thing = thing_service.get(req.params.id)
+        }
+        catch(err) {
+            res.status(404).end()
+        }
+        next();
+    });
+
+    /**
+     * Routes
+     */
+
+    // Return all data
     api.get('/', function (req, res) {
       res.json(thing_service.things)
     })
 
+    // Return the given thing data
     api.get('/:id', function (req, res) {
-      res.json(thing_service.get(req.params.id))
+      res.json(req.thing)
     })
 
+    // Return the given thing GPIOs data
     api.get('/:id/gpios', function (req, res) {
       res.json(thing_service.get(req.params.id).gpios)
     })
 
+    // Return the thing GPIO data for the given pin
     api.get('/:id/gpios/:pin', function (req, res) {
         try{
-            res.json(thing_service.get(req.params.id).gpio({"pin":req.params.pin}))
+            res.json(req.thing.gpio({"pin":req.params.pin}))
         } catch (e) {
             if (e == 'Not Found') {
                 res.status(404).send(e)
@@ -50,9 +75,10 @@ function httpService() {
         }
     })
 
+    // Return the thing GPIO value data for the given pin
     api.get('/:id/gpios/:pin/value', function (req, res) {
         try{
-            res.json({"value":thing_service.get(req.params.id).gpio({"pin":req.params.pin}).value()})
+            res.json({"value":req.thing.gpio({"pin":req.params.pin}).value()})
         } catch (e) {
             if (e == 'Not Found') {
                 res.status(404).send(e)
@@ -62,12 +88,12 @@ function httpService() {
     })
 
     api.get('/:id/actions', function (req, res) {
-        res.json(thing_service.get(req.params.id).actions)
+        res.json(req.thing.actions)
     })
 
     api.post('/:id/actions/:slug', function (req, res) {
         try{
-            thing_service.get(req.params.id).run(req.params.slug)
+            req.thing.run(req.params.slug)
             res.json({"message": "done"})
         } catch (e) {
             if (e == 'Not Found') {
@@ -78,15 +104,15 @@ function httpService() {
     })
 
     api.get('/:id/crons', function (req, res) {
-        res.json(thing_service.get(req.params.id).crons)
+        res.json(req.thing.crons)
     })
 
     api.post('/:id/crons/:slug', function (req, res) {
         try{
             if (req.query.do == 'stop') {
-                res.json(thing_service.get(req.params.id).stop(req.params.slug))
+                res.json(req.thing.stop(req.params.slug))
             } else if (req.query.do == 'start') {
-                res.json(thing_service.get(req.params.id).start(req.params.slug))
+                res.json(req.thing.start(req.params.slug))
             } else {
                 res.status(400).send('Bad Request')
             }
